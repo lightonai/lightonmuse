ff86f4336240eff55ff8a71148b3b6b784436a2c
Iacopo Poli <iacopo@lighton.io>
2022-01-19 13:39:27 +0100

    :wrench: Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import find_packages, setup
 
 name = "lightonmuse"
-description="Python client for the LightOn Muse API",
-author="LightOn AI Research",
-author_email="iacopo@lighton.ai,baptiste@lighton.ai,julien@lighton.ai",
+description = "Python client for the LightOn Muse API",
+author = "LightOn AI Research",
+author_email = "support@lighton.ai"
 classifiers = [
-                # Trove classifiers
-                # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-                "Programming Language :: Python",
-                "Programming Language :: Python :: 3",
-                "Programming Language :: Python :: 3.7",
-                "Programming Language :: Python :: 3.8",
-                "Programming Language :: Python :: 3.9",
-                "Topic :: Scientific/Engineering :: Artificial Intelligence",
-                "Development Status :: 4 - Beta",
-                "Intended Audience :: Science/Research",
-                "License :: OSI Approved :: MIT License",
-        ]
+    # Trove classifiers
+    # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: MIT License",
+]
 
 try:
     setup(
