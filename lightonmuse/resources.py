331dcbb097a1938fbf523628d316a34c96ca4af3
Iacopo Poli <iapoli@lighton.ai>
2022-04-11 15:56:04 +0200

    Update resources

--- lightonmuse/resources.py
+++ lightonmuse/resources.py
@@ -3,5 +3,5 @@ from dataclasses import dataclass
 
 @dataclass
 class Resources:
-    models = ["orion-fr", "orion-fr-v2", "lyra-en"]
-    skills = ["orion-fr@summarisation"]
+    models = ["orion-fr", "lyra-fr", "orion-en", "lyra-en", "auriga-de", "auriga-es", "auriga-it"]
+    skills = ["orion-fr@summarisation", "lyra-en@multitask", "auriga-de@zusammenfassung", "auriga-es@resumen"]
