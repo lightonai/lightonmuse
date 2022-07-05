f55696481074b19aaada4ddc11bd3468f86c3541
AmelieChatelain <amelie@lighton.io>
2022-07-05 17:27:39 +0200

    :sparkles: Implementation of CalibratedSelect

--- lightonmuse/__init__.py
+++ lightonmuse/__init__.py
@@ -1,8 +1,18 @@
 import os
 
 from .api_requests import Analyse, Compare, Create, Embed, Select, Tokenize
+from .client_side import CalibratedSelect
 
 api_key = os.environ.get("MUSE_API_KEY")
 
 
-__all__ = ["api_key", "Analyse", "Compare", "Create", "Embed", "Select", "Tokenize"]
+__all__ = [
+    "api_key",
+    "Analyse",
+    "Compare",
+    "Create",
+    "Embed",
+    "Select",
+    "Tokenize",
+    "CalibratedSelect",
+]
